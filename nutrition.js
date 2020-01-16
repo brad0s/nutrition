35d13483ac1fb38880a0991ef1d9c87ea2adddd5
braden <b@bradens-MacBook-Air-2.local>
2020-01-15 22:45:05 -0800

    Create response display - TODO

--- nutrition.js
+++ nutrition.js
@@ -34,15 +34,14 @@ searchButton.addEventListener('click', function() {
         'url': 'https://trackapi.nutritionix.com/v2/natural/nutrients',
         'method': 'POST',
         'headers': {
-        'x-app-id': APP_ID,
-        'x-app-key': APP_KEY,
-        'x-remote-user-id': '0',
-        'Content-Type':'application/x-www-form-urlencoded'
+            'x-app-id': APP_ID,
+            'x-app-key': APP_KEY,
+            'x-remote-user-id': '0',
+            'Content-Type':'application/x-www-form-urlencoded'
         },
         'processData': true,
         "data": {
-        "query": params,
-        
+            "query": params,
         },
         error:function (textstatus){ 
             console.log(textstatus); 
@@ -50,14 +49,48 @@ searchButton.addEventListener('click', function() {
         success: function(response,text,xhr) {
             console.log(response); console.log(text); console.log(xhr); 
             console.log(response.foods);
-            console.log(response.foods.length);
             let data = response.foods;
+            let list = document.getElementById('nutrition-list');
+            while(list.firstChild) {
+                list.firstChild.remove();
+            }
             for(let i=0; i<data.length; i++) {
                 console.log(data[i].length);
+                let item = data[i];
+                
+                // wrapper
                 let div = document.createElement('DIV');
                 div.classList.add('nutrition-item');
-                div.textContent = data[i].food_name;
-                document.getElementById('nutrition-list').appendChild(div);
+                
+                // img
+                let img = document.createElement('IMG');
+                img.src = item.photo.thumb;
+                img.alt = item.food_name;
+                div.appendChild(img);
+
+                // name
+                let name = document.createElement('H3');
+                name.textContent = item.food_name;
+                div.appendChild(name);
+                
+                // nutrition facts
+                let nf = document.createElement('div');
+                nf.classList.add('nutrition-facts');
+                let cal = document.createElement('DIV');
+                cal.textContent = 'Calories: ' + item.nf_calories;
+                let pro = document.createElement('DIV');
+                pro.textContent = 'Protien: ' + item.nf_protein;
+                let fat = document.createElement('DIV');
+                fat.textContent = 'Fats: ' + item.nf_total_fat;
+                let carb = document.createElement('DIV');
+                carb.textContent = 'Carbs: ' + item.nf_total_carbohydrate;
+                nf.appendChild(cal);
+                nf.appendChild(pro);
+                nf.appendChild(fat);
+                nf.appendChild(carb);
+                div.appendChild(nf);
+
+                list.appendChild(div);
             }
         }
     });
